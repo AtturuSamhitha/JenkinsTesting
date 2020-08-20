d7ef0bae20c37f2355648475b9dbfe5ed7c6f87f
samhithar123 <samhitha.a@capgemini.com>
2020-08-20 11:48:17 +0530

    Initial import of jenkins test project.

---
+++ JenkinTest/src/main/java/com/cg/App.java
@@ -0,0 +1,15 @@
+package com.cg;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+        System.out.println( "Jenkings testing!" );
+        System.out.println( "Continuous Integration!" );
+    }
+}
